33f2733befb02b911d38696eb7d75341c1bada34
Stanislava Balakhovska <stanislava_balakhovska@epam.com>
2018-09-26 16:56:53 +0300

    Added pagination and integration with database

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-URL_PATH = 'https://www.olx.ua/transport/legkovye-avtomobili/?search%5Bfilter_float_price%3Afrom%5D=1000000'
+URL_PATH = 'https://www.olx.ua/transport/legkovye-avtomobili/?search%5Bfilter_float_price%3Afrom%5D=1500000'
 
 ITEMS_LINKS_CSS = '.link.linkWithHash'
 SHOW_PHONE_NUMBER_CSS = 'span.spoiler'
@@ -6,3 +6,4 @@ PHONE_NUMBER_CSS = 'strong.xx-large'
 USER_NAME_CSS = '.offer-user__details a'
 PRICE_CSS = '.price-label strong'
 USER_SINCE_CLASS = 'user-since'
+PAGE_LINK_CSS = '.item a'
--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 from constants import *
-from sql import *
+from database import *
 
 # prepare_db()
 
@@ -14,38 +14,48 @@ chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.maximize_window()
 
+all_links_list = []
 
+create_bd()
 driver.get(URL_PATH)
 
-elem_list = driver.find_elements_by_css_selector(ITEMS_LINKS_CSS)
-links_list = []
+pages = driver.find_elements_by_css_selector(PAGE_LINK_CSS)
+pages_number = int(pages[-1].text) if pages else 1
 
+# Todo: add try-except blocks
+for i in range(1, pages_number + 1):
+    driver.get(URL_PATH + '&page={}'.format(i))
+    elem_list = driver.find_elements_by_css_selector(ITEMS_LINKS_CSS)
+    links_list = list()
 
-for element in elem_list:
-    link = element.get_attribute('href').split('#')[0]
-    links_list.append(link)
+    for element in elem_list:
+        # Todo (specially for prune) - add check for olx
+        link = element.get_attribute('href').split('#')[0]
+        links_list.append(link)
 
-for link in links_list:
+    all_links_list += filter_by_links(set(links_list))
+
+for link in all_links_list:
     driver.get(link)
-    # Todo: Add check for visited links
 
-    show_phone_number = driver.find_elements_by_css_selector(SHOW_PHONE_NUMBER_CSS)[1]
-    show_phone_number.click()
-    phone_number = driver.find_elements_by_css_selector(PHONE_NUMBER_CSS)[1].text
+    show_phone_number = driver.find_elements_by_css_selector(SHOW_PHONE_NUMBER_CSS)
+    if show_phone_number:
+        show_phone_number[1].click()
+        phone_number = driver.find_elements_by_css_selector(PHONE_NUMBER_CSS)[1].text
 
-    # Todo: Add check for phone existence in DB
+        if is_not_phone_exists(phone_number):
+            profile_link = driver.find_elements_by_css_selector(USER_NAME_CSS)[0].get_attribute('href')
+            user_name = driver.find_elements_by_css_selector(USER_NAME_CSS)[0].text.strip()
+            price = driver.find_element_by_css_selector('.price-label strong').text
+            info_line = driver.find_element_by_class_name(USER_SINCE_CLASS).text
 
-    user = driver.find_elements_by_css_selector(USER_NAME_CSS)[0].text.strip()
-    price = driver.find_element_by_css_selector('.price-label strong').text
-    line = driver.find_element_by_class_name(USER_SINCE_CLASS).text
+            insert_into_table(phone_number, user_name, link, price, profile_link, info_line)
 
-    # insert_ad_info(link, phone_number, user, price, line)
-    print link
-    print phone_number
-    print user
-    print price
-    print line
-
-    driver.back()
+            print link
+            print profile_link
+            print phone_number
+            print user_name
+            print price
+            print info_line
 
 driver.close()
